a7902f848e373b667802d05b79e0431106d2547f
Edith CaMe <edi.came040504@gmail.com>
2025-10-14 23:16:53 -0600

    Primer commit del proyecto

---
+++ src/main/java/com/example/room_rental/RoomRentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.room_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomRentalApplication.class, args);
+	}
+	// Este es el punto de entrada de mi aplicacion de springboot, es el main donde empieza a correr todo
+}
